540d980fcd858c48fff6a94f460f722ada9e70e8
Rabee Aljaramani <rabeejermani@gmail.com>
2022-03-08 12:44:50 +0400

    style

--- pages/_app.js
+++ pages/_app.js
@@ -6,10 +6,10 @@ import { StoreProvider } from '../utils/Store';
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
-    // const jssStyles = document.querySelector('#jss-server-side');
-    // if (jssStyles) {
-    //   jssStyles.parentElement.removeChild(jssStyles);
-    // }
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
   }, []);
   return (
     <SnackbarProvider anchorOrigin={{ vertical: 'top', horizontal: 'center' }}>
